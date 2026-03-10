f6b2d297c859fe4e363ce08201449cdaf7ffc880
Pornpipat <pornpipat2156@gmail.com>
2026-03-10 22:41:22 +0700

    report

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["puppeteer-core", "@sparticuz/chromium"],
+  outputFileTracingIncludes: {
+    "/*": [
+      "./node_modules/@sparticuz/chromium/**/*",
+      "./node_modules/puppeteer-core/**/*",
+    ],
+  },
 };
 
 export default nextConfig;
